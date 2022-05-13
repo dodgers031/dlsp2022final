d95ad6a6aeaff6072bc59523370d75011d1fd8ad
dodgers031 <daniel.lopez14@student.csulb.edu>
2022-05-12 20:15:45 -0700

    Version 1

--- index.js
+++ index.js
@@ -43,7 +43,76 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-// App Get Method template
-// app.get("/manage", (req, res) => {
-//     res.render("manage");
-// });
+
+//get import
+app.get("/import", async (req, res) => {
+    const totRecs = await dblib.getTotalRecords();
+    const book = {
+        book_id: "",
+        title: "",
+        total_pages: "",
+        rating: "",
+        isbn: "",
+        published_date: ""
+    };
+    res.render("import", {
+        type: "get",
+        totRecs: totRecs.totRecords,
+        book: book
+    });
+});
+// post import
+app.post("/import", upload.single('filename'), async (req, res) => {
+    const totRecs = await dblib.getTotalRecords();
+    if (!req.file || Object.keys(req.file).length === 0) {
+        message = "Error: Import file not uploaded";
+        return res.send(message);
+    };
+    //Read file line by line, inserting records
+    const buffer = req.file.buffer;
+    const lines = buffer.toString().split(/\r?\n/);
+    const model = req.body;
+    var successCount = 0;
+    var failCount = 0;
+    var errorList = [];
+    var recCount = lines.length;
+
+    lines.forEach(line => {
+        //console.log(line);
+        customer = line.split(",");
+        //console.log(customer);
+        const sql = "INSERT INTO BOOK(book_id, title, total_pages, rating, isbn, published_date ) VALUES ($1, $2, $3, $4, $5, $6)";
+        pool.query(sql, customer, (err, result) => {
+            if (err) {
+                console.log(`Insert Error.  Error message: ${err.message}`);
+                failCount++;
+                errorList.push("Book ID: ", model.book_id, " - ", err.message);
+            } else {
+                console.log(`Inserted successfully`);
+                successCount++;
+            }
+            if(recCount == (failCount+successCount)){
+                message = `Processing Complete - Processed ${recCount} records`;
+                res.render("import", {
+                    model: model,
+                    success: successCount,
+                    failed: failCount,
+                    totRecs: totRecs.totRecords,
+                    errors: errorList,
+                    type: "POST"
+                });
+            }
+        });
+    });
+});
+//get sumofseries
+
+app.get("/sumofseries", (req, res) => {
+    res.render("sumofseries");
+});
+// post sumofseries
+app.post("/sumofseries", (req, res) => {
+    res.render("sumofseries");
+    console.log("The Sum of the number from 1 to 10 incremented by 2 is 25");
+    
+});
